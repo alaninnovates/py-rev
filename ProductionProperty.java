f7beea6ea46de95a4901b429a0ccb383c5511640
alaninnovates <alanlichen1@gmail.com>
2026-05-16 20:22:48 -0700

    Some production tab properties are identified

--- ProductionProperty.java
+++ ProductionProperty.java
@@ -6,21 +6,21 @@ import ac.class_5386;
 // $VF: renamed from: i.o
 class ProductionProperty {
    // $VF: renamed from: a short
-   private short field_4961;
+   private short count;
    // $VF: renamed from: a java.lang.String
    private String field_4962;
    // $VF: renamed from: a ac.Y
    private class_5386 field_4963;
    // $VF: renamed from: b java.lang.String
-   private String field_4964;
+   private String note1;
    // $VF: renamed from: c java.lang.String
-   private String field_4965;
+   private String note2;
    // $VF: renamed from: d java.lang.String
-   private String field_4966;
+   private String note3;
    // $VF: renamed from: e java.lang.String
-   private String field_4967;
+   private String note4;
    // $VF: renamed from: f java.lang.String
-   private String field_4968;
+   private String note5;
    // $VF: renamed from: g java.lang.String
    private String field_4969;
    // $VF: renamed from: a i.l
@@ -29,15 +29,15 @@ class ProductionProperty {
 
    public ProductionProperty(Exporter3DA exporter, class_395 var2) {
       this.exporter = exporter;
-      this.field_4961 = (short)var2.field_1807; // id
+      this.count = (short)var2.field_1807; // number of counts
       this.field_4962 = var2.field_1808; //
-      this.field_4963 = var2.field_1812; //
-      this.field_4969 = var2.field_1813;
-      this.field_4964 = var2.field_1814;
-      this.field_4965 = var2.field_1815;
-      this.field_4966 = var2.field_1816;
-      this.field_4967 = var2.field_1817;
-      this.field_4968 = var2.field_1818;
+      this.field_4963 = var2.field_1812; // probably subset?
+      this.field_4969 = var2.field_1813; // probably title?
+      this.note1 = var2.field_1814;
+      this.note2 = var2.field_1815;
+      this.note3 = var2.field_1816;
+      this.note4 = var2.field_1817;
+      this.note5 = var2.field_1818;
    }
 
    // $VF: renamed from: a () void
@@ -50,24 +50,24 @@ class ProductionProperty {
          + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4962)
          + 1
          + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4969)
-         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4964)
-         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4965)
-         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4966)
-         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4967)
-         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.field_4968);
+         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.note1)
+         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.note2)
+         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.note3)
+         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.note4)
+         + Exporter3DA.staticCalculateUTF8StringSize(this.exporter, this.note5);
    }
 
    // $VF: renamed from: b () void
    private void writeData() {
-      Exporter3DA.staticWriteShortInt(this.exporter, this.field_4961);
+      Exporter3DA.staticWriteShortInt(this.exporter, this.count);
       Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4962);
       Exporter3DA.staticWriteByte(this.exporter, (byte)(this.field_4963 == class_5386.field_35120 ? 0 : 1));
       Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4969);
-      Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4964);
-      Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4965);
-      Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4966);
-      Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4967);
-      Exporter3DA.staticWriteUTF8String(this.exporter, this.field_4968);
+      Exporter3DA.staticWriteUTF8String(this.exporter, this.note1);
+      Exporter3DA.staticWriteUTF8String(this.exporter, this.note2);
+      Exporter3DA.staticWriteUTF8String(this.exporter, this.note3);
+      Exporter3DA.staticWriteUTF8String(this.exporter, this.note4);
+      Exporter3DA.staticWriteUTF8String(this.exporter, this.note5);
    }
 
    // $VF: renamed from: a (i.o) int
